9ee6bbc2a1289d3b9b337d3e113463ec13e186c2
ggang89 <ggang89@gmail.com>
2025-02-22 21:27:11 +0900

    compiler 설치하고 최적화 적용 확인하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-
-import './App.css'
-import Action from './components/Action'
-import Use from './components/use'
-import UseActionState from './components/useActionState'
-import UseOptimistic from './components/useOptimistic'
+import "./App.css";
+import Action from "./components/Action";
+import Use from "./components/use";
+import UseActionState from "./components/useActionState";
+import UseOptimistic from "./components/useOptimistic";
+import { useState } from "react";
 
 function App() {
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  console.log("App render");
 
   return (
     <>
       {/* <Use />
       <Action />
       <UseActionState /> */}
-      <UseOptimistic/>
+      {/* <UseOptimistic /> */}
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <Text text={text} />
     </>
-  )
+  );
 }
 
-export default App
+const Text = ({ text }) => {
+  console.log("Text render");
+  return <p>{text}</p>;
+}
+
+export default App;
